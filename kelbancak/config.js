31b4c21e0032457c3f56926406e59424e4585064
MTA Gentungan <mta.gentungan@gmail.com>
2026-05-25 18:09:38 +0700

    Update config.js

--- kelbancak/config.js
+++ kelbancak/config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycby4XifxI-_b4tkUtf7QBMvNbruwaHTOsOo3MJ2BPENF0l77mWj532DDFOEU2cTnIdtlXw/exec'
+  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxyv8tUzz597xyZDtOzwXax_2vOvw3GAOJd4UCvia2TEI1ubGU_3hKJzT7Eqn7-jF_acw/exec'
 }
